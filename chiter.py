47111197f02f7c1388a90c037be99e7de50e1ea8
spanchbob@gmail.com <Lkzgbnjyf777>
2018-03-16 19:23:43 +0300

    fix

--- chiter.py
+++ chiter.py
@@ -2,6 +2,7 @@ import game_b_and_c
 
 answers = game_b_and_c.get_all_answers()
 counter = 0
+bulles = 0
 while len(answers) != 1:
     counter += 1
     print('Ход номер {}'.format(counter))
@@ -9,6 +10,8 @@ while len(answers) != 1:
     ans = game_b_and_c.get_one_answer(answers)
     print('Назови комбинаци: {}'.format(ans))
     bulls = int(input('Сколько быков?'))
+    if bulls == 4:
+        break
     cows = int(input('Сколько коров?'))
     answers = game_b_and_c.del_bad_answers(answers, ans, bulls, cows)
 print('Ответ: {}'.format(answers))
