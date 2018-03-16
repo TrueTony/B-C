6dd36cac00f7dea1aed2aca136df488ddd873ea1
spanchbob@gmail.com <Lkzgbnjyf777>
2018-03-16 19:08:02 +0300

    new name

--- chiter.py
+++ chiter.py
@@ -1,14 +1,14 @@
-import game
+import game_b_and_c
 
-answers = game.get_all_answers
+answers = game_b_and_c.get_all_answers()
 counter = 0
 while len(answers) != 1:
     counter += 1
     print('Ход номер {}'.format(counter))
     print('Возможных вариантов ответа: {}'.format(len(answers)))
-    ans = game.get_one_answer(answers)
+    ans = game_b_and_c.get_one_answer(answers)
     print('Назови комбинаци: {}'.format(ans))
     bulls = int(input('Сколько быков?'))
     cows = int(input('Сколько коров?'))
-    answers = game.del_bad_answers(answers, ans, bulls, cows)
+    answers = game_b_and_c.del_bad_answers(answers, ans, bulls, cows)
 print('Ответ: {}'.format(answers))
