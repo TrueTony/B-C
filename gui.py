417fbcba60fab8a75695a2819ddfdbc2483d13bd
spanchbob@gmail.com <Lkzgbnjyf777>
2018-03-22 22:04:09 +0300

    change structure

---
+++ gui.py
@@ -0,0 +1,60 @@
+from tkinter import *
+import logic_for_gui
+
+root = Tk()
+root.title('Игра')
+# root.minsize(800,600)
+
+fram = Frame(root, width=800, height=600)
+fram.pack()
+
+
+def secondscreen():
+    btn_quit.place_forget()
+    btn_start.place_forget()
+
+    entry.place(x=275, y=260)
+    bnt_input.place(x=270, y=400)
+
+
+def thirdscreen():
+    entry.place_forget()
+    bnt_input.place_forget()
+
+    turn.place(x=330, y=170)
+    output_user.place(x=140, y=230)
+    output_comp.place(x=420, y=230)
+    try_entry.place(x=140, y=540)
+    btn_ok.place(x=325, y=540)
+    btn_sur.place(x=420, y=540)
+
+def foruthscreen():
+    pass
+
+
+
+
+# first screen
+title = Label(fram, font=('Cambria', 50), text='Быки и коровы')
+title.place(x=190, y=70)
+
+btn_start = Button(fram, width=10, text='Начать', font=('Cambria, 30'), command=secondscreen)
+btn_start.place(x=270, y=260)
+
+btn_quit = Button(fram, width=10, text='Выход', font=('Cambria, 30'), command=quit)
+btn_quit.place(x=270, y=400)
+
+# second screen
+entry = Entry(fram, width=10, font=('Cambria, 30'))
+bnt_input = Button(fram, width=10, text='Ввести', font=('Cambria, 30'), command=thirdscreen)
+
+turn = Label(fram, text='Ход NN', font=('Carambia, 20'))
+
+# third screen
+output_user = Text(fram, width=27, height=18)
+output_comp = Text(fram, width=27, height=18)
+try_entry = Entry(fram, font=('Cambria, 15'), width=10)
+btn_ok = Button(fram, text='ОК', font=('Cambria, 10'))
+btn_sur = Button(fram, text='Сдаться', font=('Cambria, 10'), width=25)
+
+root.mainloop()
---
+++ logic_for_gui.py
@@ -0,0 +1,84 @@
+import random
+
+
+# создаёт список всех возможных ответов
+def get_all_answers():
+    ans = []
+    for i in range(10000):
+        tmp = str(i).zfill(4)
+        # print(tmp)
+        # if len(set(map(int, tmp))) == 4:
+        #    ans.append(list(map(int, tmp)))
+    # print(ans)
+        lst = ['x' for num in tmp if tmp.count(num) == 1]
+        if len(lst) == 4:
+            ans.append(list(map(int, tmp)))
+    return ans
+
+# выбирает один ответ из списка возможных
+def get_one_answer(ans):
+    num = random.choice(ans)
+    return num
+
+# запрашивает упольщователя неповторяющиеся цифры
+def input_number():
+    while True:
+        nums = input('Введите 4 неповторяемые цифры: ')
+        if len(nums) != 4 or not nums.isdigit():
+            continue
+        nums = list(map(int, nums))
+        if len(set(nums)) == 4:
+            break
+    return nums
+
+# сравнивает два сисла и сообщает кол-во быков и коров
+def check(nums, true_num):
+    bulls, cows = 0, 0
+    for i, num in enumerate(nums):
+        if num in true_num:
+            if nums[i] == true_num[i]:
+                bulls += 1
+            else:
+                cows += 1
+    return bulls, cows
+
+# удаляет неподходящие варианты из списка возможных
+def del_bad_answers(ans, enemy_try, bull, cow):
+    for num in ans[:]:
+        temp_bull, temp_cow = check(num, enemy_try)
+        if temp_bull != bull and temp_bull != cow:
+            ans.remove(num)
+    return ans
+
+
+if __name__ == '__main__':
+    print('Игры Быки и Коровы')
+    answers = get_all_answers()
+    player = input_number()
+    enemy = get_one_answer(answers)
+
+    while True:
+        print('=' * 15, 'Ход игрока', '=' * 15)
+        print('Угадайте число компьютера')
+        number = input_number()
+        bulls, cows = check(number, enemy)
+        print('Быки {}, Коровы {}'.format(bulls, cows))
+        if bulls == 4:
+            print('Победил игрок!')
+            print('Компьютер загадал {}'.format(enemy))
+            break
+
+        print('=' * 15, 'Ход комьютера', '=' * 15)
+        enemy_try = get_one_answer(answers)
+        print('Компьютер считает, что вы загадали {}'.format(enemy_try))
+        bulls, cows = check(enemy_try, player)
+        print('Быки {}, Коровы {}'.format(bulls, cows))
+        if bulls == 4:
+            print('Победил компьютер!')
+            print('Компьютер загадал {}'.format(enemy))
+            break
+        else:
+            answers = del_bad_answers(answers, enemy_try, bulls, cows)
+
+
+
