8c2ca2347d6b8beab5239b46f9e2e2ce1012c6dd
spanchbob@gmail.com <Lkzgbnjyf777>
2018-03-26 16:48:32 +0300

    new tree

--- 4.py
+++ 4.py
@@ -18,15 +18,9 @@ def firscreen():
 
 
 def secondscreen():
-    trig = 0
     # запрашивает у пользователя неповторяющиеся цифры
-    nums = entry.get()
-    if len(set(nums)) == 4 and nums.isdigit():
-        trig = 1
-    else:
-        num_err.place(x=190, y=170)
-    
-    if trig == 1:
+    get_nums()
+    if nums != 0:
         num_err.place_forget()
         entry.place_forget()
         bnt_input.place_forget()
@@ -38,49 +32,87 @@ def secondscreen():
         btn_ok.place(x=325, y=540)
         btn_sur.place(x=420, y=540)
 
-        nums = list(map(int, nums))
-        return nums
-        
+        thirdscreen()
 
 
 
 def thirdscreen():
-    #while True:   #убрал цикл пока что
+    # while True:   #убрал цикл пока что
     if True:
         output_user.insert(1.0, ('=' * 5, 'Ход игрока', '=' * 5))
         output_user.insert(END, '\nУгадайте число компьютера')
-        output_user.insert(END, nums)
-        '''
-        number = input_number()
-        bulls, cows = check(number, enemy)
-        print('Быки {}, Коровы {}'.format(bulls, cows))
-        if bulls == 4:
-            print('Победил игрок!')
-            print('Компьютер загадал {}'.format(enemy))
-            break
 
-        print('=' * 15, 'Ход комьютера', '=' * 15)
-        enemy_try = get_one_answer(answers)
-        print('Компьютер считает, что вы загадали {}'.format(enemy_try))
-        bulls, cows = check(enemy_try, player)
-        print('Быки {}, Коровы {}'.format(bulls, cows))
-        if bulls == 4:
-            print('Победил компьютер!')
-            print('Компьютер загадал {}'.format(enemy))
-            break
-        else:
-            answers = del_bad_answers(answers, enemy_try, bulls, cows)
+        
+            # break
         '''
+        
+        '''
+
 
 def limit_sym(e):
     entry.delete('3', END)
     try_entry.delete('3', END)
 
-#logic
+
+def get_nums():
+    global nums
+    while True:
+        nums = entry.get()
+        if len(set(nums)) == 4 and nums.isdigit():
+            nums = list(map(int, nums))
+            print('nums before return', nums)
+            return nums
+        else:
+            num_err.place(x=190, y=170)
+            nums = 0
+            return nums
+
+def player_turn():
+    print('start get numb2')
+    nums = try_entry.get()
+    print('nums', nums)
+    if len(set(nums)) == 4 and nums.isdigit():
+        num_err.place_forget()
+        nums = list(map(int, nums))
+        print('nums before bulls, cows', nums)
+
+        bulls, cows = logic_for_gui.check(nums, enemy)
+        txt = '\nБыки {}, Коровы {}'.format(bulls, cows)
+        output_user.insert(END, txt)
+        if bulls == 4:
+            print('Победил игрок!')
+        else:
+            return
+        print('Компьютер загадал {}'.format(enemy))
+
+    else:
+        turn.place_forget()
+        num_err.place(x=190, y=170)
+
+    print(nums)
+
+def enemy_turn():
+    print('=' * 15, 'Ход комьютера', '=' * 15)
+    enemy_try = get_one_answer(answers)
+    print('Компьютер считает, что вы загадали {}'.format(enemy_try))
+    bulls, cows = check(enemy_try, player)
+    print('Быки {}, Коровы {}'.format(bulls, cows))
+    if bulls == 4:
+        print('Победил компьютер!')
+        print('Компьютер загадал {}'.format(enemy))
+        break
+    else:
+        answers = del_bad_answers(answers, enemy_try, bulls, cows)
+
+
+
+# logic
 answers = logic_for_gui.get_all_answers()
 enemy = logic_for_gui.get_one_answer(answers)
 
-#начальный nums, числа загаданное пользователем
+print(enemy)
+
+# начальный nums, числа загаданное пользователем
 nums = 0
 
 # first screen
@@ -98,9 +130,6 @@ num_err = Label(fram, text='Введите 4 неповторяющиеся ци
 entry = Entry(fram, width=10, font=('Cambria, 30'))
 entry.bind('<KeyPress>', limit_sym)
 bnt_input = Button(fram, width=10, text='Ввести', font=('Cambria, 30'), command=secondscreen)
-print(nums)
-if nums != 0:
-    thirdscreen()
 
 
 # third screen
@@ -109,7 +138,7 @@ output_user = Text(fram, width=27, height=18)
 output_comp = Text(fram, width=27, height=18)
 try_entry = Entry(fram, font=('Cambria, 15'), width=10)
 try_entry.bind('<KeyPress>', limit_sym)
-btn_ok = Button(fram, text='ОК', font=('Cambria, 10'))
+btn_ok = Button(fram, text='ОК', font=('Cambria, 10'), command=player_turn)
 btn_sur = Button(fram, text='Сдаться', font=('Cambria, 10'), width=25)
 
 root.mainloop()
