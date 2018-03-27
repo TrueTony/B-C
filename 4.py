8d7cb1178fbfeed8493541140006128c69afd7fa
TrueTony <spanchbob@gmail.com>
2018-03-27 13:26:04 +0300

    update (reverted from commit 5fd2682a7734beba64ddfd24205086b7b4d418c5)

--- 4.py
+++ 4.py
@@ -1,21 +1,15 @@
 from tkinter import Frame, Label, Entry, Tk, Text, Button, END, Toplevel
 import logic_for_gui
 
+root = Tk()
+root.title('Игра')
+# root.minsize(800,600)
 
-def firtscreen():
-    title.place(x=190, y=70)
-    btn_start.place(x=270, y=260)
-    btn_quit.place(x=270, y=400)
-
-def firstcreen_logic():
-    def trigger():
-        global trigger
-        trigger = 1
-    btn_start.config(command=trigger)
-    btn_quit.config(command=quit)
+fram = Frame(root, width=800, height=600)
+fram.pack()
 
 
-def secondscreen():
+def firscreen():
     btn_quit.place_forget()
     btn_start.place_forget()
 
@@ -23,7 +17,7 @@ def secondscreen():
     bnt_input.place(x=270, y=400)
 
 
-def thirdscreen():
+def secondscreen():
     # запрашивает у пользователя неповторяющиеся цифры
     get_player()
     if player != 0:
@@ -40,16 +34,19 @@ def thirdscreen():
 
         thirdscreen()
 
-def fourscreen():
+
+
+def thirdscreen():
     pass
 
-def fifhscreen():
+def fourscreen():
     win = Toplevel()
     win = Label(text='the End')
     win.pack
     if True:
         win.focus_force()
         win.grab_set()
+
 
 def limit_sym(e):
     entry.delete('3', END)
@@ -117,17 +114,9 @@ def enemy_turn():
         return
 
 def main():
-    firtscreen()
-    while trigger == 0:
-        # firstcreen_logic()
-        print('a')
-    #secondscreen()
+    if a == 0:
+        player_turn()
 
-root = Tk()
-root.title('Игра')
-
-fram = Frame(root, width=800, height=600)
-fram.pack()
 
 # logic
 answers = logic_for_gui.get_all_answers()
@@ -135,15 +124,18 @@ enemy = logic_for_gui.get_one_answer(answers)
 a = 0
 print('enemy', enemy)
 
-# начальный player, число загаданное пользователем
+# начальный player, числа загаданное пользователем
 player = 0
-
-trigger = 0
 
 # first screen
 title = Label(fram, font=('Cambria', 50), text='Быки и коровы')
-btn_start = Button(fram, width=10, text='Начать', font=('Cambria, 30'))
-btn_quit = Button(fram, width=10, text='Выход', font=('Cambria, 30'))
+title.place(x=190, y=70)
+
+btn_start = Button(fram, width=10, text='Начать', font=('Cambria, 30'), command=firscreen)
+btn_start.place(x=270, y=260)
+
+btn_quit = Button(fram, width=10, text='Выход', font=('Cambria, 30'), command=quit)
+btn_quit.place(x=270, y=400)
 
 # second screen
 num_err = Label(fram, text='Введите 4 неповторяющиеся цифры', font=('Carambia, 20'))
@@ -151,18 +143,15 @@ entry = Entry(fram, width=10, font=('Cambria, 30'))
 entry.bind('<KeyPress>', limit_sym)
 bnt_input = Button(fram, width=10, text='Ввести', font=('Cambria, 30'), command=secondscreen)
 
+
 # third screen
 turn = Label(fram, text='Ход NN', font=('Carambia, 20'))
 output_user = Text(fram, width=30, height=18)
 output_comp = Text(fram, width=30, height=18)
 try_entry = Entry(fram, font=('Cambria, 15'), width=10)
 try_entry.bind('<KeyPress>', limit_sym)
-btn_ok = Button(fram, text='ОК', font=('Cambria, 10'), command=None)
+btn_ok = Button(fram, text='ОК', font=('Cambria, 10'), command=main)
 btn_sur = Button(fram, text='Сдаться', font=('Cambria, 10'), width=25)
-
-
-
-
-main()
-
+if a != 0:
+    fourscreen()
 root.mainloop()
